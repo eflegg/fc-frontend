4f572572e8b1207a5d8f10911ebe44b560cb7567
Erin Flegg <ehflegg@gmail.com>
2023-03-10 11:57:51 -0800

    add in orange and pink shape files, use them in slug.tsx for demo

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -13,6 +13,21 @@ import PostTitle from '../../components/post-title'
 import Tags from '../../components/tags'
 import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
 import { CMS_NAME } from '../../lib/constants'
+import LargeOrangeShape from '../../components/shapes/large-orange'
+import styled from 'styled-components'
+import SmallPinkShape from '../../components/shapes/small-pink'
+
+const ShapeContainer = styled.div`
+background: seagreen;
+width: 100%;
+border: 2px solid yellow;
+.shape-orange {
+
+}
+.shape-pink {
+ 
+}
+`
 
 export default function Post({ post, posts, preview }) {
   const router = useRouter()
@@ -24,6 +39,10 @@ export default function Post({ post, posts, preview }) {
 
   return (
     <PageWrapper preview={preview}>
+     < ShapeContainer>
+     <SmallPinkShape  customClass="shape-pink"/>
+         <LargeOrangeShape customClass='shape-orange' />
+         </ShapeContainer>
       <Container>
         <Header />
         {router.isFallback ? (
@@ -31,6 +50,7 @@ export default function Post({ post, posts, preview }) {
         ) : (
           <>
             <article>
+           
               <Head>
                 <title>
                   {post.title} | Next.js Blog Example with {CMS_NAME}
